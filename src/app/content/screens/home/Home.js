a4c07446f51595d8a7a6cd6079b75718716dc733
Kerman Pereira <kermanpereira@gmail.com>
2024-03-14 19:39:41 -0400

    Inicializa projeto Next.js
    Este commit marca o início do desenvolvimento do projeto utilizando o framework Next.js. Foram realizadas as seguintes ações:
    - Configuração inicial do ambiente de desenvolvimento com Next.js.
    - Adição da estrutura básica do projeto, incluindo pastas e arquivos necessários.
    - Inclusão de scripts de inicialização e build no package.json.
    
    Este é o primeiro passo em direção à criação de um aplicativo web robusto com Next.js. Novas funcionalidades e melhorias serão adicionadas ao longo do desenvolvimento do projeto.

---
+++ src/app/content/screens/home/Home.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      CoffeCode
+    </main>
+  );
+}
